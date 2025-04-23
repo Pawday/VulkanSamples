3a5cd6e3ac2afca5b2a685d0d3c64a84c335104b
Pavel Korotkevich <pawday@mail.ru>
2025-04-23 22:13:08 +0300

    fix triangle-square color

--- shaders/trsq_vertex.glsl
+++ shaders/trsq_vertex.glsl
@@ -7,9 +7,9 @@ vec2 positions[] = vec2[](
 );
 
 vec3 colors[] = vec3[](
-    vec3(1, 0, 0),
-    vec3(0, 1, 0),
-    vec3(0, 0, 1)
+    vec3(0, 2, 0),
+    vec3(0, 0, 0),
+    vec3(2, 0, 0)
 );
 
 layout(location = 0) out vec3 fragColor;
