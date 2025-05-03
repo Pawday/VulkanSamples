b0cb450f1435af24f395a5d68dffa48ff89096fa
Pavel Korotkevich <pawday@mail.ru>
2025-05-03 18:36:23 +0300

    Wayland: Move ContextImpl def to header

--- wayland/ContextImpl.cc
+++ wayland/ContextImpl.cc
@@ -22,14 +22,12 @@
 #include <xdg-shell.h>
 
 #include "Context.hh"
-#include "ContextHandles.hh"
+#include "ContextImpl.hh"
 #include "RegistryListener.hh"
 #include "WindowHandles.hh"
 
 namespace Wayland {
 namespace Impl {
-
-struct ContextImpl;
 
 struct Window
 {
@@ -108,32 +106,6 @@ xdg_surface_listener Window::xdg_surface_c_vtable = []() {
 
     return output;
 }();
-
-struct ContextImpl
-{
-    friend struct Wayland::Context;
-
-    using SharedInstance = Wayland::Context::SharedInstance;
-    ContextImpl(SharedInstance I);
-    ContextImpl(const ContextImpl &) = delete;
-    ContextImpl(ContextImpl &&) = default;
-    ContextImpl &operator=(const ContextImpl &) = delete;
-    ContextImpl &operator=(ContextImpl &&) = default;
-
-    void update()
-    {
-        wl_display_dispatch(_h.display);
-    }
-
-  private:
-    ContextHandles _h;
-    RegistryListener _registry;
-
-    SharedInstance _instance;
-
-    void xdg_ping(struct xdg_wm_base *xdg_wm_base, uint32_t serial);
-    static xdg_wm_base_listener xdg_base_c_vtable;
-};
 
 xdg_wm_base_listener ContextImpl::xdg_base_c_vtable = []() {
     xdg_wm_base_listener output{};
---
+++ wayland/ContextImpl.hh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstdint>
+
+#include <wayland-client-core.h>
+#include <xdg-shell.h>
+
+#include "Context.hh"
+#include "ContextHandles.hh"
+#include "RegistryListener.hh"
+
+namespace Wayland {
+namespace Impl {
+
+struct ContextImpl
+{
+    friend struct Wayland::Context;
+
+    using SharedInstance = Wayland::Context::SharedInstance;
+    ContextImpl(SharedInstance I);
+    ContextImpl(const ContextImpl &) = delete;
+    ContextImpl(ContextImpl &&) = default;
+    ContextImpl &operator=(const ContextImpl &) = delete;
+    ContextImpl &operator=(ContextImpl &&) = default;
+
+    void update()
+    {
+        wl_display_dispatch(_h.display);
+    }
+
+  private:
+    ContextHandles _h;
+    RegistryListener _registry;
+
+    SharedInstance _instance;
+
+    void xdg_ping(struct xdg_wm_base *xdg_wm_base, uint32_t serial);
+    static xdg_wm_base_listener xdg_base_c_vtable;
+};
+
+} // namespace Impl
+} // namespace Wayland
