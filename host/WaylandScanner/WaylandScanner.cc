adf7ea7baf872e7c15ab8db2968601580be6ad6e
Pavel Korotkevich <pawday@mail.ru>
2025-05-31 20:15:40 +0300

    WaylandScanner: ENUMS Mode initial

--- host/WaylandScanner/WaylandScanner.cc
+++ host/WaylandScanner/WaylandScanner.cc
@@ -22,6 +22,7 @@
 
 #include "Format.hh"
 #include "ParseProtocol.hh"
+#include "Types.hh"
 
 namespace {
 
@@ -47,7 +48,7 @@ std::expected<JsonModeArgs, std::string>
         if (nb_json_mod_flags != 0) {
             message += " only one";
         }
-        message += " [<filename>] argument with --json flag";
+        message += " <protocol_file> argument with --json flag";
         if (nb_json_mod_flags != 0) {
             message +=
                 std::format(", got {} arguments instead", nb_json_mod_flags);
@@ -62,20 +63,115 @@ std::expected<JsonModeArgs, std::string>
     return JsonModeArgs{input_proto_filename};
 }
 
+std::string read_file(const std::string &filename)
+{
+    std::ifstream file{filename};
+    file.exceptions(std::ifstream::failbit);
+    file.exceptions(std::ifstream::badbit);
+    std::stringstream content;
+    content << file.rdbuf();
+    return content.str();
+}
+
 void process_json_mode(const JsonModeArgs &args)
 {
-    std::string protocol_xml = [&]() {
-        std::ifstream file{args.proto_file_name};
-        file.exceptions(std::ifstream::failbit);
-        file.exceptions(std::ifstream::badbit);
-        std::stringstream content;
-        content << file.rdbuf();
-        return content.str();
-    }();
+    std::string protocol_xml = read_file(args.proto_file_name);
+    auto interfaces = Wayland::parse_protocol(protocol_xml);
+    std::cout << std::format("{}\n", FormatVectorWrap{interfaces});
+}
 
+struct EnumsModeArgs
+{
+    std::string proto_file_name;
+    std::string output_file_name;
+};
+
+std::expected<EnumsModeArgs, std::string>
+    parse_enums_mode(std::vector<std::string> args)
+{
+    auto flag_it = std::ranges::find(args, "--enums");
+    if (flag_it == std::end(args)) {
+        return std::unexpected("No --enums flag");
+    }
+    args.erase(flag_it);
+
+    if (args.size() != 2) {
+        std::string message;
+        message += "Expected only";
+        message += " <protocol_file> and <output_file> arguments";
+        message += " with --enums flag";
+
+        std::vector<std::string> dec_args;
+        std::ranges::copy(args, std::back_inserter(dec_args));
+        for (auto &dec_arg : dec_args) {
+            dec_arg = std::format("({})", dec_arg);
+        }
+        FormatVectorWrap args_f{dec_args};
+
+        message += std::format(", got {} instead", args_f);
+
+        return std::unexpected(std::move(message));
+    }
+
+    EnumsModeArgs out{};
+
+    out.proto_file_name = args.at(0);
+    out.output_file_name = args.at(1);
+
+    return out;
+}
+
+std::vector<std::string> emit_enum(const Wayland::ScannerTypes::Enum &e)
+{
+    std::vector<std::string> out;
+
+    out.emplace_back(std::format("enum class {}", e.name));
+    out.emplace_back("{");
+    out.emplace_back("};");
+
+    return out;
+}
+
+void process_enums_mode([[maybe_unused]] const EnumsModeArgs &args)
+{
+    std::string protocol_xml = read_file(args.proto_file_name);
+    std::ofstream output_file{args.output_file_name};
+    output_file.exceptions(std::ifstream::failbit);
+    output_file.exceptions(std::ifstream::badbit);
     auto interfaces = Wayland::parse_protocol(protocol_xml);
 
-    std::cout << std::format("{}\n", FormatVectorWrap{interfaces});
+    std::vector<std::string> o_lines;
+    o_lines.emplace_back("#pragma once");
+    o_lines.emplace_back("");
+
+    bool first_interface = true;
+    for (auto &interface : interfaces) {
+        if (!first_interface) {
+            o_lines.emplace_back("");
+        }
+        first_interface = false;
+
+        o_lines.emplace_back(std::format("namespace {}", interface.name));
+        o_lines.emplace_back("{");
+
+        for (auto &eenum : interface.enums) {
+            auto enum_lines = emit_enum(eenum);
+
+            for (auto &enum_line : enum_lines) {
+                o_lines.emplace_back(std::move(enum_line));
+            }
+        }
+
+        o_lines.emplace_back("}");
+    }
+
+    std::string output;
+    for (auto &o_line : o_lines) {
+        output += o_line;
+        output += '\n';
+    }
+
+    output_file << output;
 }
 
 } // namespace
@@ -101,6 +197,15 @@ int Application::main()
     std::string json_mode_message =
         std::format("JSON Mode: [{}]", json_mode_args_op.error());
     failue_msgs.emplace_back(std::move(json_mode_message));
+
+    auto enums_mode_args_op = parse_enums_mode(args.argv);
+    if (enums_mode_args_op) {
+        process_enums_mode(enums_mode_args_op.value());
+        return EXIT_SUCCESS;
+    }
+    std::string enums_mode_message =
+        std::format("ENUMS Mode: [{}]", enums_mode_args_op.error());
+    failue_msgs.emplace_back(std::move(enums_mode_message));
 
     std::cout << "Failue:" << '\n';
     for (auto &msg : failue_msgs) {
