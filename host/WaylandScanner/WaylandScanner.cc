421d5d82e3ce536ba1be55c6972abbf93ba590a4
Pavel Korotkevich <pawday@mail.ru>
2025-05-31 18:52:46 +0300

    WaylandScanner: formalize args for "--json" mode

--- host/WaylandScanner/WaylandScanner.cc
+++ host/WaylandScanner/WaylandScanner.cc
@@ -1,27 +1,80 @@
+#include <algorithm>
+#include <expected>
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <optional>
+#include <set>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 #include <cstddef>
 #include <cstdlib>
 
-#include <expat.h>
-#include <expat_external.h>
+#include <string_view>
+#include <utility>
+#include <variant>
+#include <vector>
 
 #include "Application.hh"
 
 #include "Format.hh"
 #include "ParseProtocol.hh"
 
+namespace {
+
+struct JsonModeArgs
+{
+    std::string proto_file_name;
+};
+
+std::expected<JsonModeArgs, std::string>
+    parse_json_mode_args(std::vector<std::string> &args)
+{
+
+    auto json_flag_it = std::ranges::find(args, "--json");
+    if (json_flag_it == std::end(args)) {
+        return std::unexpected("No --json flag");
+    }
+
+    if (args.size() != 2) {
+        std::string message =
+            "Expected only one [<filename>] argument with --json flag";
+        message += std::format(", got {} instead", args.size() - 1);
+        return std::unexpected(std::move(message));
+    }
+    args.erase(json_flag_it);
+
+    std::string input_proto_filename = *args.begin();
+    args.clear();
+
+    return JsonModeArgs{input_proto_filename};
+}
+
+} // namespace
+
 int Application::main()
 {
-    std::string protocol_xml = [&]() {
-        auto args = get_libc_args().value();
-        std::string file_name = args.argv.at(1);
+    auto args = get_libc_args().value();
 
-        std::ifstream file{file_name};
+    if (args.argv.size() == 0) {
+        std::cout << "Hacked args";
+        return EXIT_FAILURE;
+    }
+
+    args.argv.erase(args.argv.begin());
+
+    auto json_mode_args_op = parse_json_mode_args(args.argv);
+    if (!json_mode_args_op) {
+        std::cout << "JSON Mode: " << json_mode_args_op.error() << '\n';
+        return EXIT_FAILURE;
+    }
+    auto &json_mode_args = json_mode_args_op.value();
+
+    std::string protocol_xml = [&]() {
+        std::ifstream file{json_mode_args.proto_file_name};
         file.exceptions(std::ifstream::failbit);
         file.exceptions(std::ifstream::badbit);
         std::stringstream content;
