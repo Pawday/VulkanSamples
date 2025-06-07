1a83eee4465ba3fb4952de148d8f9ea965bcfe9d
Pavel Korotkevich <pawday@mail.ru>
2025-06-07 15:49:22 +0300

    HostBox glsl: Print version and c++23

--- hostbox/GLSL.cc
+++ hostbox/GLSL.cc
@@ -1,16 +1,25 @@
+#include <format>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include <cstdlib>
 
+#include <glslang_c_interface.h>
+
 #include "GLSL.hh"
 
 int glsl_main(const std::vector<std::string> &args)
 {
-    for (auto &arg : args) {
-        std::cout << "glsl arg: [" << arg << "]\n";
-    }
+    glslang_version_t version{};
+    glslang_get_version(&version);
+
+    std::cout << std::format(
+                     "Version {}.{}.{}",
+                     version.major,
+                     version.minor,
+                     version.patch)
+              << '\n';
 
     return EXIT_FAILURE;
 }
